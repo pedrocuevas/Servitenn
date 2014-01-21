b8806f8aa22470024db5130083b33caa555798da
pedrocuevas <pcuevas24@gmail.com>
2014-01-21 01:59:50 -0300

    Modificar máquina

--- actualizarmaquina.php
+++ actualizarmaquina.php
@@ -1,11 +1,10 @@
-<?
+<?php
 include("conexion.php");
 
-if(isset($_POST['serie']) && !empty($_POST['serie']) && isset($_POST['equipo']) && !empty($_POST['equipo']) && 
-isset($_POST['motor']) && !empty($_POST['motor']) && isset($_POST['marca']) && !empty($_POST['marca']) && 
-isset($_POST['modelo']) && !empty($_POST['modelo'])) 
+if(isset($_POST['serie']) && !empty($_POST['serie']) && isset($_POST['nombre']) && !empty($_POST['nombre']) && 
+  isset($_POST['marca']) && !empty($_POST['marca']) && isset($_POST['modelo']) && !empty($_POST['modelo'])) 
 {
-	mysql_query("UPDATE Maquina SET Serie='$_POST[serie]', Equipo='$_POST[equipo]', Motor='$_POST[motor]', Marca='$_POST[marca]', Modelo='$_POST[modelo]' WHERE Serie LIKE '$_POST[serie]'",$con);
+	mysql_query("UPDATE Maquina SET serie='$_POST[serie]', nombre='$_POST[nombre]', marca='$_POST[marca]', modelo='$_POST[modelo]' WHERE serie='$_POST[serie]'",$con);
 header("Location: admin.php");
 
 }
--- modificarmaquina.php
+++ modificarmaquina.php
@@ -10,7 +10,7 @@
 	<?php
 	 include("conexion.php");
     session_start();
-    if(isset($_SESSION['username'])){
+    if(isset($_SESSION['Rut'])){
     mysql_select_db($db,$con) or die("problemas al conectar db");
     $registro = mysql_query("SELECT * FROM Maquina WHERE Serie LIKE '$_POST[serie]'") or die ("problemas en consulta:".mysql_error());
     if(mysql_num_rows($registro)==0){
@@ -27,39 +27,35 @@
                   
           <form action='actualizarmaquina.php' method='post' name='form'>
             <table>
-                 	 <?
+                 	 <?php
                  	 echo "<tr>";
 						echo	"<td>N° Serie*:</td>";
-							echo "<td><input type='text' name='serie' value='".$reg['Serie']."'required/></td>";
+							echo "<td><input type='text' name='serie' value='".$reg['serie']."'required/></td>";
 						echo "</tr>";
 						echo "<tr>";
 							echo "<td>Equipo*:</td>";
-							echo "<td><input type='text' name='equipo' value='".$reg['Equipo']."' required/></td>";
-						echo "</tr>";
-						echo "<tr>";
-							echo "<td>Motor*:</td>";
-							echo "<td><input type='text' name='motor' value='".$reg['Motor']."' required/></td>";
+							echo "<td><input type='text' name='nombre' value='".$reg['nombre']."' required/></td>";
 						echo "</tr>";
 						echo "<tr>";
 							echo "<td>Marca*:</td>";
-							echo "<td><input type='text' name='marca' value ='".$reg['Marca']."' required/></td>";
+							echo "<td><input type='text' name='marca' value ='".$reg['marca']."' required/></td>";
 						echo "</tr>";
 						echo "<tr>";
 							echo "<td>Modelo*:</td>";
-							echo "<td><input type='text' name='modelo' value='".$reg['Modelo']."' /></td>";
+							echo "<td><input type='text' name='modelo' value='".$reg['modelo']."' /></td>";
 						echo "</tr>";
 							echo "<td><input type='submit' value='insertar datos' /></td>";
 							echo "<td><a href='admin.php' ><font size='2'>volver</a></td>";
-						echo "</tr>";}?>
+						echo "</tr>";} ?>
 						</table> <br>
 				<font size='2'>*datos obligatorios</font>  <br>  
 				<br><font size='2'>Si están correctos vuelve al inicio</font>          <br>      
                  </form>
-                 <? }
+                 <?php }
                  ?>
 				</div>		
 	<?php }else {
 		echo "No puedes ver esta pagina, registrate e inicia sesion";
-		}?>
+		} ?>
 		</body>	
 </html>
