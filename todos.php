3c09c3e6594818fbaeae18b1e06d81197c47067e
pedrocuevas <pcuevas24@gmail.com>
2014-01-20 23:59:40 -0300

    Mostrar Usuarios

--- todos.php
+++ todos.php
@@ -1,10 +1,22 @@
 <!DOCTYPE html>
-<html lang="es">
-	<head>
-		<meta charset='utf-8'>
-		<link rel="stylesheet" href="estilos.css">
-		<script src='jquery.js'></script>
-        <div id='textPr'>
+<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6 lt8"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="no-js ie7 lt8"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="no-js ie8 lt8"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en" class="no-js ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html lang="es" class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="UTF-8" />
+        <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">  -->
+        <title>Login and Registration Form with HTML5 and CSS3</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <meta name="description" content="Login and Registration Form with HTML5 and CSS3" />
+        <meta name="keywords" content="html5, css3, form, switch, animation, :target, pseudo-class" />
+        <meta name="author" content="Codrops" />
+        <link rel="shortcut icon" href="../favicon.ico"> 
+        <link rel="stylesheet" type="text/css" href="css/demo.css" />
+        <link rel="stylesheet" type="text/css" href="css/style2.css" />
+		<link rel="stylesheet" type="text/css" href="css/animate-custom.css" />
+
 	</head>
 	<body>
   <?php  include("conexion.php");
@@ -13,18 +25,17 @@
     mysql_select_db($db,$con) or die("Problemas al conectar bd");
     $registro = mysql_query("SELECT * FROM Usuario ") or die ("Problemas en Consulta:".mysql_error());
 	?>
-    	<div id='contenido'>
-			<header>
-				<hgroup>
+    	<div id='container'>
+			<div id="container_demo" >
+                    <div id="wrapper">
+					 <div id="login" class="animate form">
 					<h1>Usuarios Registrados</h1>				
-				</hgroup>
-			</header>
-			</div>
+
 			<section>
 					<article>
                   <?php
                   echo"<form action='admin.php' method='post' name='form' id='todos'>";
-                 	echo"<table border=1 align='center' cellpadding='0' cellspacing='0'>";
+                 	echo"<table border=1 align='center' cellpadding='0' cellspacing='0' id='box-table-a'>";
 						echo "<tr>";
 						echo	"<td>Rut</td>";
 						echo	"<td>Nombre</td>";
@@ -40,12 +51,15 @@
 						echo "</tr>";
 					   }
 					   echo "</table>";
-                  echo "<td><input type='submit' value='volver' /></td>";					   
+                  echo "<p class='login button'><input type='submit' value='volver' /></p>";					   
 					   echo "</form>";?>
                 </article>
-				</div>	
+				
 			</section>
 		</div>
+	 </div>
+<  /div>
+</div>	
 		
 	<?php }else {
 		echo "no puedes ver esta pagina, registrate e inicia secion";
